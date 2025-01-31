ca6d21c8a6212533128a9030bc21fb5b47a12194
Syedsamid <samidsyed1720@gmail.com>
2025-01-31 11:53:50 +0000

    setup with react + Vite

---
+++ chai-aur-code/client-2-react+vite/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Myfunction from './Myfunction'
+
+function App() {
+  return (
+    <>
+    <h1>React + Vite</h1>
+  <Myfunction/>
+
+  </>
+  )
+}
+
+export default App
