912e4c758d7a33043f79cac8371eb011a2b6884a
Syedsamid <samidsyed1720@gmail.com>
2025-02-01 08:49:13 +0000

    hooks use state

--- chai-aur-code/client-2-react+vite/vite-project/src/App.jsx
+++ chai-aur-code/client-2-react+vite/vite-project/src/App.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import Myfunction from './Myfunction'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 
 function App() {
+
+  let [counter, setCounter] = useState(10)
+
+  function  addValue(){
+    console.log("value added", counter);
+    setCounter(counter + 1)
+  }
+
+   // to add more values to previous counter value use this methods interview questions
+
+  //  setCounter(preCounter => preCounter + 1)
+  //  setCounter(preCounter => preCounter + 1)
+  //  setCounter(preCounter => preCounter + 1)
+  //  setCounter(preCounter => preCounter + 1)
+
+  function remoValue (){
+    console.log('value decreased',counter);
+    setCounter(counter - 1)
+  }
   return (
     <>
-    <h1>React + Vite</h1>
-  <Myfunction/>
+
+    <h2>Code FOR India</h2>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>
+      Add value</button>
+      <br />
+
+    <button  onClick={remoValue}>Remove value</button>
+
+
 
   </>
   )
 }
-
 export default App
