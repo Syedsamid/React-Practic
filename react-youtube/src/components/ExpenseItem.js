c28efebf1060af65b0273c6cc2bdb06dbb3c345d
Syedsamid <samidsyed1720@gmail.com>
2025-02-02 14:07:45 +0000

    state in React

--- react-youtube/src/components/ExpenseItem.js
+++ react-youtube/src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import React, {useState} from "react";
 
+const ExpenseItem = (props) => {
 
-function ExpenseItem(props) {
-  
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () =>{
+    setTitle('Give New Title please ')
+  }
+
   return (
     <div className="expense-item">
      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </div>  
   );
 }
 
