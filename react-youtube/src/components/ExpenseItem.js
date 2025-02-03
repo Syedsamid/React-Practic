8cfaa348e28bf17bccef14d7eb5b3cf2b68bdb89
Syedsamid <samidsyed1720@gmail.com>
2025-02-03 05:02:36 +0000

    event handler

--- react-youtube/src/components/ExpenseItem.js
+++ react-youtube/src/components/ExpenseItem.js
@@ -1,25 +1,31 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [newTitle, setNewTitle] = useState();
 
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () =>{
-    setTitle('Give New Title please ')
+  const clickHandler = () => {
+    setTitle(newTitle);
+  };
+
+  const changeHandler = (event) =>{
+    setNewTitle(event.target.value);
   }
 
-  return (
+   return (
     <div className="expense-item">
-     <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <input type="text" value={newTitle}  onChange={changeHandler}/>
       <button onClick={clickHandler}>Change Title</button>
-    </div>  
+    </div>
   );
-}
+};
 
 export default ExpenseItem;
