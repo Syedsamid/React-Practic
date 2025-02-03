78e3946667d8047d814010dd9adde5ff0759a6e8
Syedsamid <samidsyed1720@gmail.com>
2025-02-03 05:40:52 +0000

    form

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   let expenses = [
@@ -32,26 +33,26 @@ function App() {
   return (
     <>
       <div>
-        <h2>Hi samid</h2>
+        <NewExpense/>
         <ExpenseItem
           date={expenses[0].date}
           title={expenses[0].title}
           amount={expenses[0].amount}
         ></ExpenseItem>
         <ExpenseItem
-       date={expenses[1].date}
-       title={expenses[1].title}
-       amount={expenses[1].amount}
+          date={expenses[1].date}
+          title={expenses[1].title}
+          amount={expenses[1].amount}
         ></ExpenseItem>
         <ExpenseItem
-       date={expenses[2].date}
-       title={expenses[2].title}
-       amount={expenses[2].amount}
+          date={expenses[2].date}
+          title={expenses[2].title}
+          amount={expenses[2].amount}
         ></ExpenseItem>
         <ExpenseItem
-       date={expenses[3].date}
-       title={expenses[3].title}
-       amount={expenses[3].amount}
+          date={expenses[3].date}
+          title={expenses[3].title}
+          amount={expenses[3].amount}
         ></ExpenseItem>
       </div>
     </>
--- react-youtube/src/components/ExpenseItem.js
+++ react-youtube/src/components/ExpenseItem.js
@@ -3,27 +3,14 @@ import ExpenseDate from "./ExpenseDate";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [newTitle, setNewTitle] = useState();
-
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle(newTitle);
-  };
-
-  const changeHandler = (event) =>{
-    setNewTitle(event.target.value);
-  }
-
+  
    return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <input type="text" value={newTitle}  onChange={changeHandler}/>
-      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 };
